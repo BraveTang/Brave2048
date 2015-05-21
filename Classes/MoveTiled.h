e194b6d9982730b27f3ae5ca11ce3433229b7dd3
brave <tangongjun6@gmail.com>
2015-05-21 22:27:00 +0800

    Revert b72b11c..040fbb3
    This rolls back to commit b72b11c7bf65eb3d0ebd2c7e11c544676ee477e5.

--- Classes/MoveTiled.h
+++ Classes/MoveTiled.h
@@ -13,16 +13,10 @@
 
 class MoveTiled:public cocos2d::Node
 {
-private:
+public:
     int m_row;
     int m_col;
     int m_number;
-    cocos2d::Label* label;
-    cocos2d::LayerColor* bk;
-public:
-    int getNumber()const;
-    void setBackGroud(const int number);
-    void setNumber(const int num);
     void showAt(int r, int c);//在某个位置显示某个块
     void moveTo(int r, int c);//移动到r行，c列
     CREATE_FUNC(MoveTiled);
